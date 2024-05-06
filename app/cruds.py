1ff386f026e421e2cd4cb4e40ca3d48c7147ab41
Kentaro Kato <ken.k.jul2@gmail.com>
2024-05-06 09:50:58 +0900

    [fix] import

--- app/cruds.py
+++ app/cruds.py
@@ -1,12 +1,20 @@
 from sqlalchemy.orm import Session
-from app import models, schemas
+from app.models import user_model
+from app.schema import user_schema
 
 
 def get_user(db: Session, user_id: int):
-    return db.query(models.User).filter(models.User.id == user_id).first()
+    # return db.query(user.User).filter(user.User.id == user_id).first()
+    return 123
 
-def create_user(db: Session, user: schemas.UserBase):
-    new_user = models.User(name=user.name)
+
+def get_user_by_name(db: Session, user_name: str):
+    # return db.query(user.User).filter(user.User.name == user_name).first()
+    return 123
+
+
+def create_user(db: Session, user: user_schema.UserBase):
+    new_user = user_model.User(name=user.name)
     db.add(new_user)
     db.commit()
     db.refresh(new_user)
